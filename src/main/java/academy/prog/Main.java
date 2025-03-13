9230aedeabbd79c639637a892cc8a1c8ff4e18f8
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-03-14 00:47:19 +0100

    Initial commit: added authentification by password, added private messages

---
+++ src/main/java/academy/prog/Main.java
@@ -0,0 +1,64 @@
+package academy.prog;
+
+import academy.prog.models.Message;
+import academy.prog.models.User;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+	public static String FindName(String message) {
+		Pattern pattern = Pattern.compile("@([a-zA-Z0-9_]+)");
+		Matcher matcher = pattern.matcher(message);
+
+		if (matcher.find()) {
+			return matcher.group(1); // find name after @
+		}
+		return null;
+	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		try {
+			System.out.print("Enter your login: ");
+			String login = scanner.nextLine();
+			System.out.print("Enter your password: ");
+			String pass = scanner.nextLine();
+
+			User usr = new User(login, pass);
+
+			int answerUserCheck = usr.send(Utils.getURL() + "/check");
+			if (answerUserCheck == 200) { // 200 OK
+				Thread th = new Thread(new GetThread(login));
+				th.setDaemon(true);
+				th.start();
+
+				System.out.print("\nEnter your message: ");
+				while (true) {
+					String text = scanner.nextLine();
+					if (text.isEmpty()) break;
+
+					String loginTo = FindName(text);
+
+					Message m = new Message(login, loginTo, text);
+					int res = m.send(Utils.getURL() + "/add");
+
+					if (res != 200) { // 200 OK
+						System.out.println("HTTP error occurred: " + res);
+						return;
+					}
+				}
+			}else{
+				System.out.println("HTTP error occurred: " + answerUserCheck);
+				usr.send(Utils.getURL() + "/status");
+				System.out.println("User "+usr.toString());
+				return;
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			scanner.close();
+		}
+	}
+}
---
+++ src/main/java/academy/prog/models/User.java
@@ -0,0 +1,64 @@
+package academy.prog.models;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class User {
+
+    private String login;
+    private String password;
+
+//    private StatusType status;
+
+    public User(String name, String password){
+        this.login = login;
+        this.password = password;
+    }
+
+    public String toJSON() {
+        Gson gson = new GsonBuilder().create();
+        return gson.toJson(this);
+    }
+
+    public static User fromJSON(String s) {
+        Gson gson = new GsonBuilder().create();
+        return gson.fromJson(s, User.class);
+    }
+
+//    public StatusType getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(StatusType status) {
+//        this.status = status;
+//    }
+
+    public int send(String url) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        try (OutputStream os = conn.getOutputStream()) {
+            String json = toJSON();
+            os.write(json.getBytes(StandardCharsets.UTF_8));
+            return conn.getResponseCode(); // 200?
+        }
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append("[").append(login)
+//                .append(", ").append(password)
+//                .append(", ").append(status)
+                .append("] ")
+                .toString();
+    }
+}
