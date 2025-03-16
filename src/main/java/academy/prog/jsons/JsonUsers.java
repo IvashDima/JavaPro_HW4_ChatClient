6d6b1237528408edd757b575ab0ae84f60ec9f1a
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-03-17 00:03:37 +0100

    Deleted JsonResponse & unused old code.

--- src/main/java/academy/prog/jsons/JsonUsers.java
+++ src/main/java/academy/prog/jsons/JsonUsers.java
@@ -6,13 +6,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-//import academy.prog.enums.StatusType;
 public class JsonUsers {
     private final List<User> list = new ArrayList<>();
 
-    public JsonUsers(List<User> sourceList) { //, StatusType status
+    public JsonUsers(List<User> sourceList) {
         for (int i = 0; i < sourceList.size(); i++){
-//            if (sourceList.get(i).getStatus() == status)
                 list.add(sourceList.get(i));
         }
     }
